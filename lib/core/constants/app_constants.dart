f52d95c6c8a79b718ecb51c9286f291d2f1ff48e
kartikeya <kartikeya.199231@gmail.com>
2023-06-19 03:51:32 +0530

    Add new api token

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,4 +2,4 @@ const kAppDescription = 'Read the top headlines from the world today';
 
 /// For demo purposes
 /// This API token is hardcoded here
-const devNewsApiToken = 'oxBK0FytCkLIbJOxthzb98GdSBdsXWsTH6A67nlW';
+const devNewsApiToken = 'GY3WZDPFghoUaBycQVYw0PxDTBBbVBYicw9BT5tX';
